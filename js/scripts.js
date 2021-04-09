2eb57a1b332c57b768b99697870b5314903fb27c
yesthecarlos <carlosmendez86@gmail.com>
2021-04-09 15:31:52 -0700

    add medium and large options to getPrice function

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ function Order(size, toppings, price) {
 
 Order.prototype.getPrice = function () {
   const pizzaSize = this.size
-  const numberOfToppings = this.toppings.length - 1
+  const numberOfToppings = this.toppings.length
     if (pizzaSize === "small" && numberOfToppings === 1) {
       this.price = 10
     } else if (pizzaSize === "small" && numberOfToppings === 2) {
@@ -17,6 +17,22 @@ Order.prototype.getPrice = function () {
     } else if (pizzaSize === "small" && numberOfToppings === 3) {
       this.price = 14
     } else if (pizzaSize === "small" && numberOfToppings > 3) {
+      alert("3 is the maximum number of toppings")
+    } else if (pizzaSize === "medium" && numberOfToppings === 1) {
+      this.price = 13
+    } else if (pizzaSize === "medium" && numberOfToppings === 2) {
+      this.price = 15
+    } else if (pizzaSize === "medium" && numberOfToppings === 3) {
+      this.price = 17
+    } else if (pizzaSize === "medium" && numberOfToppings > 3) {
+      alert("3 is the maximum number of toppings")
+    } else if (pizzaSize === "large" && numberOfToppings === 1) {
+      this.price = 16
+    } else if (pizzaSize === "large" && numberOfToppings === 2) {
+      this.price = 18
+    } else if (pizzaSize === "large" && numberOfToppings === 3) {
+      this.price = 20
+    } else if (pizzaSize === "large" && numberOfToppings > 3) {
       alert("3 is the maximum number of toppings")
     } else {
     alert("Enter something")
@@ -26,3 +42,14 @@ Order.prototype.getPrice = function () {
 
 let order1 = new Order("small",["pepperoni", "sausage"],0);
     
+let order2 = new Order("small", ["pepperoni"], 0);
+
+let order3 = new Order("small", ["pepperoni", "sausage", "artichoke", "olives"], 0);
+
+let order4 = new Order("medium", ["pepperoni", "sausage"], 0);
+
+let order5 = new Order("medium", ["pepperoni", "sausage", "artichoke", "olives"], 0);
+
+let order6 = new Order("large", ["pepperoni", "sausage", "artichoke", "olives"], 0);
+
+let order7 = new Order("large", ["pepperoni", "sausage"], 0);
