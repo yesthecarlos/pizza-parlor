03cefcbf0ccaca8eed80803c77cded20e9e01cc2
yesthecarlos <carlosmendez86@gmail.com>
2021-04-09 13:11:52 -0700

    WIP: setting up getPrice function

--- js/scripts.js
+++ js/scripts.js
@@ -10,19 +10,18 @@ function Order(size, toppings, price, customerName, orderID) {
 Order.prototype.getPrice = function () {
   let pizzaSize = this.Order.size
   let numberOfToppings = array.length(this.Order.toppings)
-    if (pizzaSize === small && numberOfToppings === 1) {
+    if (pizzaSize === "small" && numberOfToppings === 1) {
       this.price = 10
-    } else if {
-    pizzaSize === small && numberOfToppings === 2) {
+    } else if (pizzaSize === "small" && numberOfToppings === 2) {
       this.price = 12  
-    } else if {
-    pizzaSize === small && numberOfToppings === 3) {
+    } else if (pizzaSize === "small" && numberOfToppings === 3) {
       this.price = 14
-    } else if {
-    pizzaSize === small && numberOfToppings > 3) {
+    } else if (pizzaSize === "small" && numberOfToppings > 3) {
       alert("3 is the maximum number of toppings")
+    } else {
+    alert("Enter something")
     }
     }
 
-    }
-}
+let order1 = new Order("small", ["pepperoni", "sausage"])
+    
